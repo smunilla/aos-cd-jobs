c8a6fc6d161f444f972792a7b1986cf821d1160c
Troy Dawson <tdawson@redhat.com>
2017-04-05 16:43:34 -0500

    Check if already done

--- rcm-guest/mirrors-stage-to-prod.sh
+++ rcm-guest/mirrors-stage-to-prod.sh
@@ -12,5 +12,5 @@ else
   BOT_USER=""
 fi
 
-ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com "LASTDIR=$(readlink /srv/enterprise/online-stg/latest) ; echo ${LASTDIR} ; cd /srv/enterprise/online-prod/ ;  cp -r --link ../online-stg/${LASTDIR} ${LASTDIR} ; rm -f latest ; ln -s ${LASTDIR} latest"
+ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com 'LASTDIR=$(readlink /srv/enterprise/online-stg/latest) ; echo ${LASTDIR} ; cd /srv/enterprise/online-prod/ ;  if [ -d ${LASTDIR} ] ; then echo Already Done; else cp -r --link ../online-stg/${LASTDIR} ${LASTDIR} ; rm -f latest ; ln -s ${LASTDIR} latest ; fi'
 ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com /usr/local/bin/push.enterprise.sh -v
