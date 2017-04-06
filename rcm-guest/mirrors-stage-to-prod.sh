ab8948639497da65352e34e8f50e4460c7ff4a3c
Troy Dawson <tdawson@redhat.com>
2017-04-06 10:22:48 -0500

    Move stg to prod for libra

--- rcm-guest/mirrors-stage-to-prod.sh
+++ rcm-guest/mirrors-stage-to-prod.sh
@@ -14,3 +14,6 @@ fi
 
 ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com 'LASTDIR=$(readlink /srv/enterprise/online-stg/latest) ; echo ${LASTDIR} ; cd /srv/enterprise/online-prod/ ;  if [ -d ${LASTDIR} ] ; then echo Already Done; else cp -r --link ../online-stg/${LASTDIR} ${LASTDIR} ; rm -f latest ; ln -s ${LASTDIR} latest ; fi'
 ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com /usr/local/bin/push.enterprise.sh -v
+
+ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com 'LASTDIR=$(date +%Y-%m-%d) ; echo ${LASTDIR} ; cd /srv/libra/online-prod/ ;  if [ -d ${LASTDIR} ] ; then echo Already Done; else mkdir -p ${LASTDIR}/x86_64/ ; cp -r --link ../rhel-7-libra-stage/x86_64/ ${LASTDIR}/x86_64/os ; rm -f latest ; ln -s ${LASTDIR} latest ; fi'
+ssh ${BOT_USER} -o StrictHostKeychecking=no use-mirror-upload.ops.rhcloud.com /usr/local/bin/push.libra.sh online-prod -v
